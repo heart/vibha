8636fff17531bb24c81eab98f0f01809245acc00
jamemini <msaechoa@gmail.com>
2021-03-24 10:49:31 +0700

    add new img

--- components/VibhaVideo.vue
+++ components/VibhaVideo.vue
@@ -38,17 +38,59 @@ export default class VibhaVideo extends Vue {}
     width: 60%;
     height: 90%;
 }
-@media screen and (max-width: 768px) {
-.vibha-video{
-    width: 80%;
-    height: 80%;
+@media screen and (max-width: 1600px) {
+  .vibhavideo-container{
+    height:  830px   ;
 }
+}
+@media screen and (max-width: 1400px) {
+  .vibhavideo-container{
+    height:  750px   ;
+}
+}
+@media screen and (max-width: 1300px) {
+  .vibhavideo-container{
+    height:  650px   ;
+}
+}
+@media screen and (max-width: 1200px) {
+  .vibhavideo-container{
+    height:  650px   ;
+}
+}
+@media screen and (max-width: 1100px) {
+  .vibhavideo-container{
+    height:  550px   ;
+}
+}
+@media screen and (max-width: 900px) {
+  .vibhavideo-container{
+    height:  450px   ;
+}
+}
+@media screen and (max-width: 700px) {
+  .vibhavideo-container{
+    height:  400px   ;
+}
+}
+@media screen and (max-width: 600px) {
+  .vibhavideo-container{
+    height:  350px   ;
+}
+}
+@media screen and (max-width: 500px) {
+  .vibhavideo-container{
+    height:  300px   ;
+}
+}
+@media screen and (max-width: 400px) {
+  .vibhavideo-container{
+    height:  250px   ;
+}
+}
+@media screen and (max-width: 375px) {
 .vibhavideo-container{
-    width: 100%;
-    height: 300px   ;
-}
-.vibhavideo-center{
-    padding-top: 13%;
+    height:  230px   ;
 }
 }
 </style>
